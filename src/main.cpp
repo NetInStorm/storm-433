0fd2028d9a61e22c2b1b1645523bb1348f9aeeac
Daniel Solmann <19241189+dsolmann@users.noreply.github.com>
2020-06-06 19:29:03 +0300

    Fixing strings...

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,13 @@
 #include "FS.h"
 
 /* TODO: Load from FS */
-const char *ssid = "InStorm-ZYHP5L";
+const char *ssid = "InStorm-20";
 const char *password = "demo_password";
 
 AsyncWebServer server(80); // Setting up API Web-server
 
 RH_NRF905 nrf905(5, 4, 15); // Initializing NRF905 Radio Driver
-RHMesh mesh(nrf905, 21); // ADDR fix
+RHMesh mesh(nrf905, 20); // ADDR fix
 
 IPAddress local_IP(192,168,1,1); // 192.168.1.x, cuz 10.x.x.x would be later used in modern versions in routing.
 IPAddress gateway(192,168,1,1);
@@ -48,19 +48,31 @@ void setup() {
   /* Setting up HTTP server */
   
   server.on("/random", HTTP_GET, [](AsyncWebServerRequest *request){
+    Serial.println("[Wi-Fi] Request on /random");
     request->send(200, "text/plain", String(random(1000))); // Just testing programmability of a server. DELETE later. TODO
   });
 
   server.on("/api/v1/send", HTTP_POST, [](AsyncWebServerRequest *request){
     uint8_t addr;
-    u_char buf[RH_MESH_MAX_MESSAGE_LEN];
-    
+    u_char *buf;
+    String data;
+    Serial.println("[Wi-Fi] Request on /api/v1/send");
+
     if (request->hasParam("addr", true) && request->hasParam("body", true)) {
+        Serial.println("[Wi-Fi/RadioLogic] Params are present");
         addr = request->getParam("addr", true)->value().toInt();
-        request->getParam("body", true)->value().getBytes(buf, RH_MESH_MAX_MESSAGE_LEN);
+        Serial.print("[Wi-Fi/RadioLogic] Sending message to ");
+        Serial.println(addr);
+        data = request->getParam("body", true)->value().c_str();
+        Serial.print("[Wi-Fi/RadioLogic] Message is ");
+        Serial.println(data);
+        strcpy((char*) buf, data.c_str()); 
+        Serial.println("[Wi-Fi/RadioLogic] Message encoded and ready to send");
         if (mesh.sendtoWait(buf, sizeof(buf), addr) == RH_ROUTER_ERROR_NONE) {
+          Serial.println("[Wi-Fi/RadioLogic] Sending failed");
           request->send(500, "text/plain", "fail");
         } else {
+          Serial.println("[Wi-Fi/RadioLogic] Sending success");
           request->send(200, "text/plain", "OK");
         }
     } else {
@@ -69,6 +81,7 @@ void setup() {
   });
 
   server.on("/api/v1/poll", HTTP_GET, [](AsyncWebServerRequest *request){
+    Serial.println("[Wi-Fi] Request on /api/v1/poll");
     uint8_t l_buf[RH_MESH_MAX_MESSAGE_LEN];
     if (q.pop(&l_buf)) {
       std::string s(l_buf, l_buf + sizeof(l_buf));
@@ -80,6 +93,7 @@ void setup() {
 
 
   server.on("/api/v1/name", HTTP_GET, [](AsyncWebServerRequest *request){
+    Serial.println("[Wi-Fi] Request on /api/v1/name");
     request->send(200, "text/plain", String(ssid));
   });
 
@@ -117,6 +131,8 @@ void loop() {
 
   if (mesh.recvfromAck(l_buf, &len, &from))
   {
+    Serial.print("[RadioLogic] Message from ");
+    Serial.println(from);
     q.push(l_buf);
   }
 }
